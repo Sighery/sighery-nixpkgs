28fdc2145bfb1b21f5fc70cfdcaa84648131b3e6
Sighery <11218602+Sighery@users.noreply.github.com>
2026-09-15 04:41:31 +0200

    WIP: more flake.nix pkgs definition

--- flake.nix
+++ flake.nix
@@ -9,24 +9,8 @@
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
-      packagesOverlay = final: prev: {
-        audio-notification = final.callPackage ./pkgs/audio-notification { };
-        brightness-notification = final.callPackage ./pkgs/brightness-notification { };
-        ffmpeg-helpers = final.callPackage ./pkgs/ffmpeg-helpers { };
-        hermes = final.callPackage ./pkgs/hermes { };
-        irlserver-irl-srt-server = final.callPackage ./pkgs/irlserver-irl-srt-server { };
-        irlserver-srt = final.callPackage ./pkgs/irlserver-srt { };
-        irlserver-srtla = final.callPackage ./pkgs/irlserver-srtla { };
-        kitty-grab = final.callPackage ./pkgs/kitty-grab { };
-        openirl-srt = final.callPackage ./pkgs/openirl-srt { };
-        openirl-srt-live-server = final.callPackage ./pkgs/openirl-srt-live-server { };
-        openirl-srtla = final.callPackage ./pkgs/openirl-srtla { };
-        scrcpy-rofi = final.callPackage ./pkgs/scrcpy-rofi { };
-        spotify-adblock = final.callPackage ./pkgs/spotify-adblock { };
-        vineflower = final.callPackage ./pkgs/vineflower { };
-        vscode-antislop-settings = final.callPackage ./pkgs/vscode-antislop-settings { };
-        xclipboard = final.callPackage ./pkgs/xclipboard { };
-      };
+      packagesOverlay = final: prev:
+        import ./pkgs final;
 
       spotifyOverride = import ./overrides/spotify.nix;
       fantasqueOverride = import ./overrides/fantasque-sans-mono.nix;
@@ -43,24 +27,7 @@
             overlays = [ overlay ];
           };
         in
-        {
-          audio-notification = pkgs.audio-notification;
-          brightness-notification = pkgs.brightness-notification;
-          ffmpeg-helpers = pkgs.ffmpeg-helpers;
-          hermes = pkgs.hermes;
-          irlserver-irl-srt-server = pkgs.irlserver-irl-srt-server;
-          irlserver-srt = pkgs.irlserver-srt;
-          irlserver-srtla = pkgs.irlserver-srtla;
-          kitty-grab = pkgs.kitty-grab;
-          openirl-srt = pkgs.openirl-srt;
-          openirl-srt-live-server = pkgs.openirl-srt-live-server;
-          openirl-srtla = pkgs.openirl-srtla;
-          scrcpy-rofi = pkgs.scrcpy-rofi;
-          spotify-adblock = pkgs.spotify-adblock;
-          vineflower = pkgs.vineflower;
-          vscode-antislop-settings = pkgs.vscode-antislop-settings;
-          xclipboard = pkgs.xclipboard;
-        };
+        import ./pkgs pkgs;
     in
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
